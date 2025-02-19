6564ba8b5ce0526834a092f718231e982ad00dd7
MetaMechanists <35176119+LordIdra@users.noreply.github.com>
2025-02-19 18:11:48 +0000

    Improve challenge examples to be more modern and use flask_wtf

--- challenge/__init__.py
+++ challenge/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 # app.config.from_object('config')
+
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 from challenge import views  # noqa: F401 E402
---
+++ challenge/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
